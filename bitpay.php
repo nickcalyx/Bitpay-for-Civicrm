91dcc6e274f967df6358c0e5dfe1a21a51f0b2a2
Matthew Wire <mjw@mjwconsult.co.uk>
2022-06-25 12:58:43 +0100

    Update civix

--- bitpay.php
+++ bitpay.php
@@ -15,15 +15,6 @@ function bitpay_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function bitpay_civicrm_xmlMenu(&$files) {
-  _bitpay_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -77,54 +68,6 @@ function bitpay_civicrm_disable() {
  */
 function bitpay_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _bitpay_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function bitpay_civicrm_managed(&$entities) {
-  _bitpay_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function bitpay_civicrm_caseTypes(&$caseTypes) {
-  _bitpay_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function bitpay_civicrm_angularModules(&$angularModules) {
-  _bitpay_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function bitpay_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _bitpay_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
