1aa076ae7ef331b8efc9ce859f8cbd42572b1575
Matthew Wire <mjw@mjwconsult.co.uk>
2022-07-01 22:03:10 +0100

    Add duplicate checking for webhooks

--- CRM/Core/Payment/Bitpay.php
+++ CRM/Core/Payment/Bitpay.php
@@ -256,6 +256,24 @@ class CRM_Core_Payment_Bitpay extends CRM_Core_Payment {
    * Return TRUE for success, FALSE if there's a problem
    */
   public function processWebhookEvent(array $webhookEvent) :bool {
+    // If there is another copy of this event in the table with a lower ID, then
+    // this is a duplicate that should be ignored. We do not worry if there is one with a higher ID
+    // because that means that while there are duplicates, we'll only process the one with the lowest ID.
+    $duplicates = PaymentprocessorWebhook::get(FALSE)
+      ->selectRowCount()
+      ->addWhere('event_id', '=', $webhookEvent['event_id'])
+      ->addWhere('trigger', '=', $webhookEvent['trigger'])
+      ->addWhere('id', '<', $webhookEvent['id'])
+      ->execute()->count();
+    if ($duplicates) {
+      PaymentprocessorWebhook::update(FALSE)
+        ->addWhere('id', '=', $webhookEvent['id'])
+        ->addValue('status', 'error')
+        ->addValue('message', 'Refusing to process this event as it is a duplicate.')
+        ->execute();
+      return FALSE;
+    }
+
     $handler = new CRM_Core_Payment_BitpayIPN($this);
     $result = $handler->processQueuedWebhookEvent($webhookEvent);
 
